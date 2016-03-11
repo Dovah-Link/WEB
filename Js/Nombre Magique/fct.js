ed5d19861b092638980a78f953b8207bf79dc2bc
DovahLink <vince.sene@gmail.com>
2016-03-11 09:32:03 +0100

    Version fonctionnel sans bug du Nombre Magique

--- Js/Nombre Magique/fct.js
+++ Js/Nombre Magique/fct.js
@@ -3,6 +3,7 @@
 function nombremag()
 {
 	NbSecret = Math.floor(Math.random() * 101);
+	console.log(NbSecret);
 }
 
 function search()
@@ -17,30 +18,32 @@ function search()
 	// }
 	if(NbSecret!=0)
 	{
-		if(re==true)
+		if(re.test(Nb.value)==true)
 		{
-			if(Nb.value>NbSecret)
+			if(parseInt(Nb.value)>NbSecret)
 			{
 				resultat="Trop grand !";
 				document.getElementById("label1").style.visibility="visible";
-				document.getElementById("label1").style.backgroundColor="#d9edf7";
+				document.getElementById("label1").className="alert alert-warning";
 			}
-			else if(Nb.value<NbSecret)
+			else if(parseInt(Nb.value)<NbSecret)
 			{
 				resultat="Trop petit !";
 				document.getElementById("label1").style.visibility="visible";
-				document.getElementById("label1").style.backgroundColor="#d9edf7";
+				document.getElementById("label1").className="alert alert-warning";
 			}
 			else
 			{
 				resultat="Félicitation vous avez trouvez le nombre magique : "+NbSecret;
+				document.getElementById("label1").style.visibility="visible";
+				document.getElementById("label1").className="alert alert-success";
 			}
 		}
 		else
 		{
-			resultat="Erreur de saisie."
-			document.getElementById("label1").style..visibility="visible";
-			document.getElementById("label1").style.backgroundColor="#f2dede";
+			resultat="Erreur de saisie.";
+			document.getElementById("label1").style.visibility="visible";
+			document.getElementById("label1").className="alert alert-danger";
 		}
 	}
 	label1.innerHTML = resultat;
@@ -52,7 +55,7 @@ var resultat;
 var btn = document.getElementById("ok");
 var label1 = document.getElementById("label1");
 var Nb = document.getElementById('Nb');
-var re = /[0-9]{1,3}/
+var re = /[0-9]{1,3}/;
 
 window.addEventListener("load", nombremag);
 btn.addEventListener("click", search);
